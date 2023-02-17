e66840af4c6510b33fbe09a9e26a617d81736376
iamaamunir <abdullahimunir3@gmail.com>
2023-02-17 12:14:38 +0100

    fix: data model for notes

---
+++ models/noteModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String
+  },
+  content: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+  },
+  owner: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
+    },
+  ],
+});
+
+module.exports = mongoose.model("Notes", noteSchema);
