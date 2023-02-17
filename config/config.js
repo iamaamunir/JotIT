d33f07266ce989e6b703c834d46061f82f322050
iamaamunir <abdullahimunir3@gmail.com>
2023-02-17 03:46:07 +0100

    fix: added node_env and token_key

--- config/config.js
+++ config/config.js
@@ -3,5 +3,7 @@ require("dotenv").config();
 const CONFIG = {
   PORT: process.env.PORT,
   MONGODB_URL: process.env.MONGODB_URL,
+  NODE_ENV: process.env.NODE_ENV,
+  TOKEN_KEY: process.env.TOKEN_KEY,
 };
 module.exports = CONFIG;
