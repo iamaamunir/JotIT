4f172c401c99c2c7f625e63cd10dca9f3e21e017
iamaamunir <abdullahimunir3@gmail.com>
2023-02-17 12:04:03 +0100

    fix:user token verification

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const CONFIG = require("../config/config");
+
+exports.verifyToken = (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+
+  if (!token) {
+    return res.status(403).send("A token is required for authentication");
+  }
+  try {
+    const decoded = jwt.verify(token, CONFIG.TOKEN_KEY);
+    req.user = decoded;
+  } catch (err) {
+    return res.status(401).send("Invalid Token");
+  }
+  return next();
+};
