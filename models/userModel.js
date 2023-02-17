8a2d7274116b5980b8aa426c912c6ccc867af62f
iamaamunir <abdullahimunir3@gmail.com>
2023-02-17 03:43:38 +0100

    fix: user model/ password hashing

---
+++ models/userModel.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please tell us your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please fill in your email address"],
+    validate: [validator.isEmail, "Please provide a valid email address"],
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please fill in your password"],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please fill in to confirm your password"],
+    // validate passwordd
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords don't match!!!",
+    },
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+  // Only hash the password if it has been modified or is new
+  if (!this.isModified("password") && !this.isModified("passwordConfirm")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  // Hash the passwordConfirm
+  this.passwordConfirm = await bcrypt.hash(this.passwordConfirm, salt);
+
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.path("passwordConfirm").validate(function (value) {
+  return this.password === value;
+}, "Passwords do not match");
+
+module.exports = mongoose.model("User", userSchema);
