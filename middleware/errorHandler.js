b1f9ed36a9b3b841f183a4b646402441fca48aa4
Munir Abdullahi <abdullahimunir3@gmail.com>
2023-02-20 13:27:42 +0100

    fix:error handlding

---
+++ middleware/errorHandler.js
@@ -0,0 +1,11 @@
+exports.errorHandler = function (err, req, res, next) {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  // Send an error response to the client
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+  next();
+};
